42fe9e3669c21179ee4373e7136bf000cc24b0d0
RonRahaman <rahaman@cs.uchicago.edu>
2020-10-03 11:41:20 -0500

    Used abcs

--- my_abs.py
+++ my_abs.py
@@ -1,10 +1,13 @@
 import math
+from numeric.abc import Real, Complex
 
 def my_abs(x):
-    try:
+    if isinstance(x, Real):
         if x < 0:
             return -x
         else:
             return x
-    except TypeError:
+    else if isinstance(x, Complex):
+        do that math i forget
+    else:
         return math.nan
